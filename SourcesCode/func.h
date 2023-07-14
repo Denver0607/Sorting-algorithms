e73c75941f495eb650b5234936f2eb8c75fdd4bb
nguyenvmthien <114507572+nguyenvmthien@users.noreply.github.com>
2023-07-14 23:12:33 +0700

    Update func.h

--- SourcesCode/func.h
+++ SourcesCode/func.h
@@ -1,9 +1,1 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <string.h>
-#include <fstream>
-#include <stdlib.h>
-#include "DataGenerator.cpp"
-using namespace std;
-
+/*Xóa file này =)))*/
