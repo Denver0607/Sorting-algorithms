68a91e6a7f6a22ec7ceba360c16dfcec618e39d2
NTienne <126379060+NTienne@users.noreply.github.com>
2023-07-17 22:12:13 +0700

    Update selectionSort.cpp

--- SourcesCode/sorting-algorithm/selectionSort.cpp
+++ SourcesCode/sorting-algorithm/selectionSort.cpp
@@ -1,53 +1,48 @@
 #include "..\header.h"
 // reference: codelearn.io
 
-void selectionSort(int* a, int n){
-	int indexMin;
-	for (int i = 0; i < n-1; i++){
-		indexMin = i;
-		for (int j = i+1; j < n; j++){
-			if (a[indexMin] > a[j]){
-				indexMin = j;
-			}
-		}
-		if (i != indexMin){
-			int temp = a[i];
-			a[i] = a[indexMin];
-			a[indexMin] = temp;
-		}
-	}
-} 
-
 // Run Time  
-void selectionSortRunTime(int* a, int n)
+void selectionSortRunTime(int*& arr, int n, long long & time)
 {
 	// Start time
  	auto start = chrono::high_resolution_clock::now();
 
-    selectionSort(a, n);
-
-    // End time
-    auto end = chrono::high_resolution_clock::now();
-	// Print out
-	auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    cout << "Time taken by Selection Sort: " << duration << " microseconds" << endl;
-}
-
-// Comparison counting
-void selectionSortCompare(int* a, int n, int& count){
 	int indexMin;
-	for (int i = 0; ++count && i < n-1; i++){
+	for (int i = 0; i < n-1; i++){
 		indexMin = i;
-		for (int j = i+1; ++count && j < n; j++){
-			if (++count && a[indexMin] > a[j]){
+		for (int j = i+1; j < n; j++){
+			if (arr[indexMin] > arr[j]){
 				indexMin = j;
 			}
 		}
-		if (++count && i != indexMin){
-			int temp = a[i];
-			a[i] = a[indexMin];
-			a[indexMin] = temp;
+		if (i != indexMin){
+			int temp = arr[i];
+			arr[i] = arr[indexMin];
+			arr[indexMin] = temp;
+		}
+	}
+
+    // End time
+	auto end = chrono::high_resolution_clock::now();
+	
+	time=chrono::duration_cast<chrono::microseconds>(end - start).count();
+    
+}
+
+// Comparison counting
+void selectionSortCompare(int*& arr, int n, long long & count_comparisons){
+	int indexMin;
+	for (int i = 0; ++count_comparisons && i < n-1; i++){
+		indexMin = i;
+		for (int j = i+1; ++count_comparisons && j < n; j++){
+			if (++count_comparisons && arr[indexMin] > arr[j]){
+				indexMin = j;
+			}
+		}
+		if (++count_comparisons && i != indexMin){
+			int temp = arr[i];
+			arr[i] = arr[indexMin];
+			arr[indexMin] = temp;
 		}
 	}
 } 
-
