fe4663f8021ece35d53483fab99ddcbeda4f18d6
NTienne <126379060+NTienne@users.noreply.github.com>
2023-07-17 22:20:31 +0700

    Update mergeSort.cpp

--- SourcesCode/sorting-algorithm/mergeSort.cpp
+++ SourcesCode/sorting-algorithm/mergeSort.cpp
@@ -1,7 +1,7 @@
 #include "..\header.h"
 // reference: codelearn.io
 
-void merge(int *arr, int l, int m, int r)
+void merge(int*&arr, int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
@@ -46,34 +46,32 @@ void merge(int *arr, int l, int m, int r)
         k++;
     }
 }
-void mergeSort(int *a, int l, int r)
+void mergeSort(int*& arr, int l, int r)
 {
     if (l < r)
     {
         int m = l + (r - l) / 2;
-        mergeSort(a, l, m);
-        mergeSort(a, m + 1, r);
-        merge(a, l, m, r);
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
 // Run Time
-void mergeSortRunTime(int *a, int n)
+void mergeSortRunTime(int*& arr, int n, long long& time)
 {
     // Start time
     auto start = chrono::high_resolution_clock::now();
 
-    mergeSort(a, 0, n - 1);
+    mergeSort(arr, 0, n - 1);
 
     // End time
     auto end = chrono::high_resolution_clock::now();
-    // Print out
-    auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    cout << "Time taken by Merge Sort: " << duration << " microseconds" << endl;
+	time = chrono::duration_cast<chrono::microseconds>(end - start).count();
 }
 
 // Comparison counting
-void mergeCount(int *arr, int l, int m, int r, int &count)
+void mergeCount(int *arr, int l, int m, int r, long long &count)
 {
     int i, j, k;
     int n1 = m - l + 1;
@@ -118,20 +116,20 @@ void mergeCount(int *arr, int l, int m, int r, int &count)
         k++;
     }
 }
-void mergeSortCount(int *a, int l, int r, int &count)
+void mergeSortCount(int*& arr, int l, int r, long long &count)
 {
     if (++count && l < r)
     {
         int m = l + (r - l) / 2;
-        mergeSortCount(a, l, m, count);
-        mergeSortCount(a, m + 1, r, count);
-        mergeCount(a, l, m, r, count);
+        mergeSortCount(arr, l, m, count);
+        mergeSortCount(arr, m + 1, r, count);
+        mergeCount(arr, l, m, r, count);
     }
 }
 
-void mergeSortCompare(int *a, int n, int &count)
+void mergeSortCompare(int*& arr, int n, long long &count_comparisons)
 {
     int l = 0;
     int r = n - 1;
-    mergeSortCount(a, l, r, count);
+    mergeSortCount(arr, l, r, count_comparisons);
 }
