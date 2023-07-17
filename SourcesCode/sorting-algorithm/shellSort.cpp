31b48ea54c16e28e2fdc155057bf10f537baa4c4
NTienne <126379060+NTienne@users.noreply.github.com>
2023-07-17 22:02:59 +0700

    Update shellSort.cpp

--- SourcesCode/sorting-algorithm/shellSort.cpp
+++ SourcesCode/sorting-algorithm/shellSort.cpp
@@ -1,46 +1,41 @@
 #include "..\header.h"
 
-void shellSort(int* a, int n){
-	int interval, i, j, temp;
-	for(interval = n/2; interval > 0; interval /= 2){
-		for(i = interval; i < n; i++){
-			temp = a[i];
-			for(j = i; j >= interval && a[j - interval] > temp; j -= interval){
-				a[j] = a[j - interval];				
-			}
-			a[j] = temp;
-		}
-    }
-}
-
 //Run Time
-void shellSortRunTime(int* a , int n)
+void shellSortRunTime(int*& arr , int n, long long& time)
 {
 	// Start time
  	auto start = chrono::high_resolution_clock::now();
 
-    shellSort(a, n);
+   	int interval, i, j, temp;
+	for(interval = n/2; interval > 0; interval /= 2){
+		for(i = interval; i < n; i++){
+			temp = arr[i];
+			for(j = i; j >= interval && arr[j - interval] > temp; j -= interval){
+				arr[j] = arr[j - interval];				
+			}
+			arr[j] = temp;
+		}
+    }
 
     // End time
     auto end = chrono::high_resolution_clock::now();
 	// Print out
-	auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    cout << "Time taken by Shell Sort: " << duration << " microseconds" << endl;
+	time = chrono::duration_cast<chrono::microseconds>(end - start).count();
 }
 
 //Comparison counting
-void shellSortCompare(int* a, int n, int& count)
+void shellSortCompare(int*& arr, int n, long long & count_comparisons)
 {
 	int interval, i, j, temp;
-	for(interval = n/2; ++count && interval > 0; interval /= 2)
+	for(interval = n/2; ++count_comparisons && interval > 0; interval /= 2)
 	{
-		for(i = interval; ++count && i < n; i++)
+		for(i = interval; ++count_comparisons && i < n; i++)
 		{
-			temp = a[i];
-			for(j = i; ++count && j >= interval && ++count && a[j - interval] > temp; j -= interval){
-				a[j] = a[j - interval];				
+			temp = arr[i];
+			for(j = i; ++count_comparisons && j >= interval && ++count_comparisons && arr[j - interval] > temp; j -= interval){
+				arr[j] = arr[j - interval];				
 			}
-			a[j] = temp;
+			arr[j] = temp;
 		}
     }
 }
